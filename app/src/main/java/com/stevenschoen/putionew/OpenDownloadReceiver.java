fd6e029c3a1077b2d69a92277fb0990ef3235e55
Steven Schoen <steven@stevenschoen.com>
2014-06-19 19:49:38 -0400

    Fixed ongoing download notification doing nothing when clicked

---
+++ app/src/main/java/com/stevenschoen/putionew/OpenDownloadReceiver.java
@@ -0,0 +1,16 @@
+package com.stevenschoen.putionew;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class OpenDownloadReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent dm = new Intent(DownloadManager.ACTION_VIEW_DOWNLOADS);
+		dm.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		context.startActivity(dm);
+	}
+}
